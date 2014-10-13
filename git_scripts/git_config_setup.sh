58fb857923783bd588e048579383ed270cc0f617
Donny Redmond <dredmond@cloud816.com>
2014-10-13 11:18:49 -0500

    Fixed a bug in the git config setup script.

--- git_scripts/git_config_setup.sh
+++ git_scripts/git_config_setup.sh
@@ -29,14 +29,14 @@ printf "Installing Global VS 2013 MergeTool\n"
 $git_config merge.tool vsdiffmerge
 $git_config mergetool.prompt false
 $git_config mergetool.vsdiffmerge.cmd "'C:/Program Files (x86)/Microsoft Visual Studio 12.0/Common7/IDE/vsdiffmerge.exe' \$REMOTE \$LOCAL \$BASE \$MERGED //m"
-$git_config mergetool.vsdiffmerge.keepbackup false
-$git_config mergetool.vsdiffmerge.trustexitcode true
+$git_config mergetool.vsdiffmerge.keepBackup false
+$git_config mergetool.vsdiffmerge.trustExitCode true
 
 printf "Installing Global VS 2013 DiffTool\n"
 $git_config diff.tool vsdiffmerge
 $git_config difftool.prompt false
 $git_config difftool.vsdiffmerge.cmd "'C:/Program Files (x86)/Microsoft Visual Studio 12.0/Common7/IDE/vsdiffmerge.exe' \$LOCAL \$REMOTE //t"
-$git_config difftool.vsdiffmerge.keepbackup false
-$git_config difftool.vsdiffmerge.trustexitcode true
+$git_config difftool.vsdiffmerge.keepBackup false
+$git_config difftool.vsdiffmerge.trustExitCode true
 
 printf "\n\033[1;32m==================================================\033[0m\n\033[0;32mFinished Installing Global VSDiffMerge Settings!!!\033[0m\n\n"
